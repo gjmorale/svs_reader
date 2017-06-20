608b5d7a64847f7091fb75023f6632ded5e7e35c
finantec developer <finantec.operaciones@gmail.com>
2017-06-20 11:03:47 -0400

    Date compatible with hyphen and omission of mutual funds

--- dev/DateCue.rb
+++ dev/DateCue.rb
@@ -5,13 +5,15 @@ class DateCue
 	attr_reader :day
 
 	def initialize str
+		str = str.gsub('-','/')
 		vars = str.split('/')
 		day = vars[0].to_i
 		month = vars[1].to_i
 		year = vars[2].to_i
+		puts "#{day};#{month};#{year}"
 		@day = (day < 10 ? "0#{day}" : day.to_s)
 		@month = (month < 10 ? "0#{month}" : month.to_s)
-		@year = year.to_s
+		@year = (2000 + year).to_s
 	end
 
 	def to_s
--- preciosSVS.rb
+++ preciosSVS.rb
@@ -64,58 +64,63 @@ class Reader
 
 		f_output.write("tipo;codigo;fecha;valor;p_cierre;serie\n")
 		instruments.each.with_index do |instrument, j|
+			puts instrument
 			print "[#{progress j, instruments.size}]."
 			b.goto instrument.url
 			# ingresar fecha inicio
 			print "..."
-			b.select_list(:name, "dia1").select_value(date_i.day)
-			b.select_list(:name, "mes1").select_value(date_i.month)
-			b.select_list(:name, "anio1").select_value(date_i.year)
-			# ingresar fecha fin
-			b.select_list(:name, "dia2").select_value(date_f.day)
-			b.select_list(:name, "mes2").select_value(date_f.month)
-			b.select_list(:name, "anio2").select_value(date_f.year)
-			#Apretar boton envio
-			print ".."
-			b.button(:name => 'sub_consulta_fi').click
+			begin
+				b.select_list(:name, "dia1").select_value(date_i.day)
+				b.select_list(:name, "mes1").select_value(date_i.month)
+				b.select_list(:name, "anio1").select_value(date_i.year)
+				# ingresar fecha fin
+				b.select_list(:name, "dia2").select_value(date_f.day)
+				b.select_list(:name, "mes2").select_value(date_f.month)
+				b.select_list(:name, "anio2").select_value(date_f.year)
+				#Apretar boton envio
+				print ".."
+				b.button(:name => 'sub_consulta_fi').click
 
-			#sleep 1
-			print ".."
-			Watir::Wait.until { b.dl(id: "datos_ent").present? }
-			print "....."
+				#sleep 1
+				print ".."
+				Watir::Wait.until { b.dl(id: "datos_ent").present? }
+				print "....."
 
-			#sacamos los datos de la tabla
-			i_date = i_serie = i_value = -1
-			b.table.ths.each.with_index do |th, i|
-				case th.text
-				when "Fecha"
-					i_date = i
-				when "Serie"
-					i_serie = i
-				when "Valor Económico"
-					i_value = i
-				end
-					
-			end
-			print ".."
-			rows = b.table.rows
-			rows.each.with_index do |row, i|
-				unless row.tds.size == 0
-					serie = instrument.get_serie(row.tds[i_serie].text)
-					if serie
-						serie.positions << Position.new(row.tds[i_date].text, row.tds[i_value].text) 
-					else
-						@f_log.write("Skipped serie #{row.tds[i_serie].text} for #{instrument.code} beacuse it didn't match #{instrument.series.first.name}\n")
+				#sacamos los datos de la tabla
+				i_date = i_serie = i_value = -1
+				b.table.ths.each.with_index do |th, i|
+					case th.text
+					when "Fecha"
+						i_date = i
+					when "Serie"
+						i_serie = i
+					when "Valor Económico"
+						i_value = i
 					end
-					print "." if (i-1) % ((rows.size-1)/4) == 0 if rows.size >= 4
+						
 				end
+				print ".."
+				rows = b.table.rows
+				rows.each.with_index do |row, i|
+					unless row.tds.size == 0
+						serie = instrument.get_serie(row.tds[i_serie].text)
+						if serie
+							serie.positions << Position.new(row.tds[i_date].text, row.tds[i_value].text) 
+						else
+							@f_log.write("Skipped serie #{row.tds[i_serie].text} for #{instrument.code} beacuse it didn't match #{instrument.series.first.name}\n")
+						end
+						print "." if (i-1) % ((rows.size-1)/4) == 0 if rows.size >= 4
+					end
+				end
+				if rows.size < 4
+					print "...."
+				end
+				print ".."
+				instrument.print @f_output, @f_log
+				puts "..[Done] #{instrument}"
+			rescue StandardError => e
+				print "... FONDO MUTUO"
 			end
-			if rows.size < 4
-				print "...."
-			end
-			print ".."
-			instrument.print @f_output, @f_log
-			puts "..[Done] #{instrument}"
 		end
 	end
 end
